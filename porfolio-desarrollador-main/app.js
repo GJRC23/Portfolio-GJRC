3da8262d21899b3e436094946abc0b5225209b57
GJRC23 <gastuconsoli8@gmail.com>
2024-11-15 00:47:39 -0300

    Cambio funciona bien menú proyectos

--- porfolio-desarrollador-main/app.js
+++ porfolio-desarrollador-main/app.js
@@ -24,67 +24,6 @@ function descargarCV(idioma) {
     document.body.removeChild(link);
 }
 
-
-//funciòn que carga las animaciones de las habilidades
-function cargarAnimaciones(){
-    $(function(){
-        $('.chart1').easyPieChart({
-            size:160,
-            barColor:"#ff5b00",
-            scaleLength:0,
-            lineWidth:15,
-            tackColor:"#525151",
-            lineCap:"circle",
-            animate:2000,
-        });
-        $('.chart2').easyPieChart({
-            size:160,
-            barColor:"#ff5b00",
-            scaleLength:0,
-            lineWidth:15,
-            tackColor:"#525151",
-            lineCap:"circle",
-            animate:2000,
-        });
-        $('.chart3').easyPieChart({
-            size:160,
-            barColor:"#ff5b00",
-            scaleLength:0,
-            lineWidth:15,
-            tackColor:"#525151",
-            lineCap:"circle",
-            animate:2000,
-        });
-        $('.chart4').easyPieChart({
-            size:160,
-            barColor:"#ff5b00",
-            scaleLength:0,
-            lineWidth:15,
-            tackColor:"#525151",
-            lineCap:"circle",
-            animate:2000,
-        });
-        $('.chart5').easyPieChart({
-            size:160,
-            barColor:"#ff5b00",
-            scaleLength:0,
-            lineWidth:15,
-            tackColor:"#525151",
-            lineCap:"circle",
-            animate:2000,
-        });
-        $('.chart6').easyPieChart({
-            size:160,
-            barColor:"#ff5b00",
-            scaleLength:0,
-            lineWidth:15,
-            tackColor:"#525151",
-            lineCap:"circle",
-            animate:2000,
-        });
-
-    })
-}
 //Función para filtrar las categorías de los trabajos
 function verCategoria(cat){
     const items = document.getElementsByClassName("item");
@@ -102,31 +41,10 @@ function verCategoria(cat){
     links[1].className = "";
     links[2].className = "";
     links[3].className = "";
+    links[4].className = "";
 
     const itemSeleccionado = document.getElementById(cat);
     itemSeleccionado.className = "borde";
-}
-
-//Función que detecta cuando lanzar la animación de las habilidades
-function efectoHabilidades(){
-    var skills = document.getElementById("habilidades");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("chart");
-        habilidades[0].classList.add("chart1");
-        habilidades[1].classList.add("chart2");
-        habilidades[2].classList.add("chart3");
-        habilidades[3].classList.add("chart4");
-        habilidades[4].classList.add("chart5");
-        habilidades[5].classList.add("chart6");
-
-        cargarAnimaciones();
-    }
-}
-
-//detecto el scrolling para aplicar la animación de las habilidaes
-window.onscroll = function(){
-    efectoHabilidades();
 }
 
 //función que muestra el menu responsive{
